271cba1b76ea14fd053e4d9cb9455a28ea6a3c8f
tyler-gilmore <tdg016@gmail.com>
2025-09-16 00:55:43 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -26,22 +26,60 @@ $(function () {
     // ONLY CHANGE BELOW THIS POINT //
     //////////////////////////////////
 
-    // TODO 1 - Enable the Grid
-    // toggleGrid();
+    // Enable the Grid
+    toggleGrid();
 
 
-    // TODO 2 - Create Platforms
+    // Create Platforms
+    createPlatform(100, 0, 10, 600);
+    function createTrench(xStart, yStart) {
+      const PLATFORM_WIDTH = 100;
+      const PLATFORM_HEIGHT = 10;
+      const TRENCH_HEIGHT = 50;
+      let x = xStart;
+      let y = yStart;
 
+      createPlatform(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT);
+      
+      x += PLATFORM_WIDTH;
+      y -= PLATFORM_WIDTH;
+      createPlatform(x, y, PLATFORM_HEIGHT, PLATFORM_WIDTH); // swapped width and height for vertical wall
 
+      createPlatform(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT);
 
+      x += PLATFORM_WIDTH;
+      createPlatform(x, y, PLATFORM_HEIGHT, PLATFORM_WIDTH); // swapped width and height for vertical wall
+    }
+
+    for (let i = 0; i < 5; i++) {
+      createTrench(100 + i * 200, 600);
+    }
+
+    createPlatform(1100, 100, 10, 400);
+
+    createPlatform(1300, 600, 100, 10);
+    createPlatform(1300, 400, 100, 10);
+    createPlatform(1300, 200, 100, 10);
+
+    createPlatform(1100, 500, 100, 10);
+    createPlatform(1100, 300, 100, 10);
+    createPlatform(1100, 100, 100, 10);
 
     // TODO 3 - Create Collectables
-
+    for (let i = 0; i < 5; i++) {
+      createCollectable('diamond', 240 + i * 200, 510);
+    }
+    for (let i = 0; i < 5; i++) {
+      createCollectable('database', 140 + i * 200, 510);
+    }
+    
 
 
     
     // TODO 4 - Create Cannons
-
+    createCannon('right', 750, 2000);
+    createCannon('top', 1310, 2000);
+    createCannon('left', 375, 2000);
 
     
     
