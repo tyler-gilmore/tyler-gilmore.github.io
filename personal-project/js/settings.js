6933009eca5b04959cb61efe3795f79dbc2d645e
tyler-gilmore <tdg016@gmail.com>
2025-09-22 15:41:32 +0000

    Started settings creation

--- personal-project/js/settings.js
+++ personal-project/js/settings.js
@@ -1,14 +1,14 @@
 export class Settings {
   get playerAspectRatio() {
     const img = new Image();
-    img.src = '../assets/images/player.png'
+    img.src = '../assets/images/player.png';
     return img.onload = function() {
       const aspectRatio = img.naturalWidth / img.naturalHeight;
       return aspectRatio;
     }
   }
   constructor(board) {
-    this.relativeSpriteSize = 0.10 // size relative to screen size
+    this.relativeSpriteSize = 0.10; // size relative to screen size
     this.updateScreenSize(board);
   }
   updateScreenSize(board) {
