10dc8ef13ca1fb0f221dd2f9ef1a67494af1272b
tyler-gilmore <tdg016@gmail.com>
2025-11-11 01:23:22 +0000

    Runtime: TODO 1 - 4

--- fsd-projects/runtime/js/view/background.js
+++ fsd-projects/runtime/js/view/background.js
@@ -29,6 +29,12 @@ var background = function (window) {
         // ANIMATION VARIABLES HERE //////////////////////////////////////
         //////////////////////////////////////////////////////////////////
         // TODO (several):
+        let tree;
+        let moon;
+        let grass;
+        let buildings = [];
+
+        const GRASS_Y_OFFSET = 50;
       
       
         // called at the start of game and whenever the page is resized
@@ -39,17 +45,46 @@ var background = function (window) {
             // TODO 1:
             // this currently fills the background with an obnoxious yellow;
             // you should modify both the height and color to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
+            const backgroundFill = draw.rect(canvasWidth,canvasHeight,'#181f2b');
             background.addChild(backgroundFill);
             
             // TODO 2: - Add a moon and starfield
-            
+            const STAR_COUNT = 200;
+            for (let i = 0; i < STAR_COUNT; i++) {
+                const star = draw.circle(3, 'white', 'LightGray', 2);
+                star.x = canvasWidth * Math.random();
+                star.y = canvasHeight * Math.random();
+                background.addChild(star);
+            }
+
+            moon = draw.bitmap("img/moon.png");
+            moon.x = canvasWidth * 0.6;
+            moon.y = canvasHeight * 0.01;
+            moon.scaleX = 0.3;
+            moon.scaleY = 0.3;
+            background.addChild(moon);
+
+            // Add grass
+            grass = draw.rect(canvasWidth, canvasHeight - groundY + GRASS_Y_OFFSET, '#1a2b18', 1);
+            grass.y = groundY - GRASS_Y_OFFSET;
+            background.addChild(grass);
             
             // TODO 4: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
-            
+            const numberOfBuildings = 15;
+            for (let i = 0; i < numberOfBuildings; i++) {
+                const buildingHeight = Math.random() * 200 + 100;
+                const building = draw.rect(75, buildingHeight, "DarkGray", "Black", 1);
+                building.x = canvasWidth / (numberOfBuildings - 1) * i;
+                building.y = groundY - 20 - buildingHeight - (Math.random() * (GRASS_Y_OFFSET - 20));
+                background.addChild(building);
+                buildings.push(building);
+            }
             
             // TODO 3: Part 1 - Add a tree
-            
+            tree = draw.bitmap('img/tree.png');
+            tree.x = canvasWidth - 100;
+            tree.y = groundY - 240;
+            background.addChild(tree);
             
         } // end of render function - DO NOT DELETE
         
@@ -63,10 +98,18 @@ var background = function (window) {
             var groundY = ground.y;
             
             // TODO 3: Part 2 - Move the tree!
-            
+            tree.x -= 1;
+            if (tree.x < -200) {
+                tree.x = canvasWidth;
+            }
             
             // TODO 4: Part 2 - Parallax
-            
+            for (let building of buildings) {
+                building.x -= 0.1;
+                if (building.x < -building.width) {
+                    building.x = canvasWidth + building.width;
+                } 
+            }
 
         } // end of update function - DO NOT DELETE
         
